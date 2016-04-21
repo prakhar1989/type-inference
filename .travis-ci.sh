bf23d314ffd05ff52e006b53bf38e7d20eb2db25
Prakhar Srivastav <prakhar1989@gmail.com>
2016-04-21 18:30:16 -0400

    trying to fix travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,10 +2,6 @@
 OPAM_DEPENDS="ocamlfind alcotest"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
-  3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
-  3.12.1,1.1.0) ppa=avsm/ocaml312+opam11 ;;
-  4.00.1,1.0.0) ppa=avsm/ocaml40+opam10 ;;
-  4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
   4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
   4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
   *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
