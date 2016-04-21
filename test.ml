13e2462d63f87100fbbb8764fd31278d0cf58280
Prakhar Srivastav <prakhar1989@gmail.com>
2016-04-21 19:37:30 -0400

    finished the test suite

--- test.ml
+++ test.ml
@@ -27,15 +27,19 @@ let debug (e: expr): string =
 let testcases = [|
   NumLit(10);
   BoolLit(true);
+
   Binop(NumLit(10), Add, NumLit(2));
   Binop(BoolLit(true), Or, BoolLit(false));
+
   Binop(Binop(Val("x"), Add, Val("y")), Mul, Val("z"));
   Binop(Binop(Val("x"), Add, Val("y")), Gt, Val("z"));
   Binop(Binop(Val("x"), Gt, Val("y")), Lt, Val("z"));
   Binop(Binop(Val("x"), Mul, Val("y")), Lt, Binop(Val("z"), Add, Val("w")));
-  Binop(Binop(Val("x"), Gt, Val("y")), Lt, Binop(Val("z"), Lt, Val("w")));
+
   Fun("x", Binop(Val("x"), Add, NumLit(10)));
   Fun("x", Binop(NumLit(20), Gt,Binop(Val("x"), Add, NumLit(10))));
+
+  App(Fun("x", Binop(Val("x"), Add, NumLit(10))), NumLit(10));
   Fun("f", Fun("g", Fun("x", App(Val("f"), App(Val("g"), Val("x"))))));
 |];;
 
@@ -53,10 +57,36 @@ let simple_expr_check () =
   end
 ;;
 
+let var_expr_check () =
+  begin
+    Alcotest.(check string) "x + y + z" "int" (debug testcases.(4));
+    Alcotest.(check string) "x + y > z" "bool" (debug testcases.(5));
+    Alcotest.(check string) "(x > y) < z" "bool" (debug testcases.(6));
+    Alcotest.(check string) "(x * y) < (z + w)" "bool" (debug testcases.(7));
+  end
+;;
+
+let func_decl_check () =
+  begin
+    Alcotest.(check string) "fun x -> x + 10" "(int -> int)" (debug testcases.(8));
+    Alcotest.(check string) "fun x -> (20 > (x + 10))" "(int -> bool)" (debug testcases.(9));
+  end
+;;
+
+
+let func_appl_check () =
+  begin
+    Alcotest.(check string) "(fun x -> x + 10) 10" "int" (debug testcases.(10));
+    Alcotest.(check string) "compose function" "(('a -> 'b) -> (('c -> 'a) -> ('c -> 'b)))" (debug testcases.(11));
+  end
+;;
 
 let infer_set = [
   "Literals", `Quick, literals_check;
   "Simple Expr", `Quick, simple_expr_check;
+  "Variable Expr", `Quick, var_expr_check;
+  "Function Declarations", `Quick, func_decl_check;
+  "Function Application", `Quick, func_appl_check;
 ]
 
 (* Run it *)
